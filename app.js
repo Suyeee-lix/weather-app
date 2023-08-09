2f8b5bd099de9a3626393e67c94903531338ce5f
Suyeee-lix <141268874+Suyeee-lix@users.noreply.github.com>
2023-08-09 15:58:41 +0630

    display Temperature

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
+function formatDate(timestamp) {
+  let now = new Date(timestamp);
 
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Firday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = days[now.getDay()];
+  let minutes = now.getMinutes();
+  let hours = now.getHours();
+
+  return `${day} ${hours}: ${minutes}`;
+}
+
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let descriptionElement = document.querySelector("#description");
+  let dateElement = document.querySelector("#date");
+
+  console.log(response.data.time);
+
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  descriptionElement.innerHTML = response.data.condition.description;
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+}
+
+let apiKey = "f4406d0fa0ff8b4act21f580342802od";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New York&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
